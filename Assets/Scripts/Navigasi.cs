a0318bf5ba6080548a99562f897cf7d747e33efd
unknown <simple.ilham@gmail.com>
2019-09-28 23:25:51 -0700

    menambahkan tombol navigasi

---
+++ Assets/Scripts/Navigasi.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Navigasi : MonoBehaviour
+{
+   void Retry()
+   {
+       Application.LoadLevel(Application.loadedLevel);
+   }
+
+   void Keluar()
+   {
+       Application.Quit();
+   }
+}
